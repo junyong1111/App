ea27fcac8b6fd31a736449d8824a85b26c7cdfe0
Junyong <jypark93@kookmin.ac.kr>
2022-08-23 22:14:59 +0900

    add:finish_dart

--- Flutter/flutter_store/lib/main.dart
+++ Flutter/flutter_store/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
