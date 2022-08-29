0bcc68c00a76608a27e6e8693e478be5d8c12ea7
Junyong <jypark93@kookmin.ac.kr>
2022-08-29 14:51:49 +0900

    add:flutter

--- Flutter/flutter_yolo/lib/Pages/home_page.dart
+++ Flutter/flutter_yolo/lib/Pages/home_page.dart
@@ -37,7 +37,7 @@ class _HomePageState extends State<HomePage> {
           //   ),
           //   textAlign: TextAlign.center,
           // ),
-          FireTimer(),
+          // FireTimer(),
         ],
       ),
     );
