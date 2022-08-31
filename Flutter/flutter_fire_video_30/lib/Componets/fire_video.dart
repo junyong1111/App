9d5681de24d7e9f85987c8afc524f808cd474f8a
Junyong <jypark93@kookmin.ac.kr>
2022-08-31 18:36:26 +0900

    add:flutter_firebase

--- Flutter/flutter_fire_video_30/lib/Componets/fire_video.dart
+++ Flutter/flutter_fire_video_30/lib/Componets/fire_video.dart
@@ -1,9 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_30/Componets/video_play.dart';
 
-class FireVideo extends StatelessWidget {
+class FireVideo extends StatefulWidget {
+  @override
+  State<FireVideo> createState() => _FireVideoState();
+}
+
+class _FireVideoState extends State<FireVideo> {
   @override
   Widget build(BuildContext context) {
+    _buildDb(); //--- 최초 1회 실행
     return Column(
       children: [
         Text(
@@ -13,12 +20,18 @@ class FireVideo extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(top: 15.0),
           child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: VideoPlayerScreen(),
-          ),
+              borderRadius: BorderRadius.circular(20),
+              child: VideoPlayerScreen()),
         ),
         // 일단 경로를 넣어줘야 될듯
       ],
     );
+  }
+
+  Future<void> _buildDb() async {
+    var db = FirebaseFirestore.instance;
+    var doc_ref = await db.collection("Video").doc('FIRE2022년 08월 30일_6').get();
+    print("====================================================");
+    print(doc_ref.data());
   }
 }
--- Flutter/flutter_fire_video_30/lib/main.dart
+++ Flutter/flutter_fire_video_30/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_30/Pages/human_page.dart';
@@ -12,7 +13,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(MyApp());
 }
 
