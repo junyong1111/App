728735f6dbf4d787d435671005565ff027121441
Junyong <jypark93@kookmin.ac.kr>
2022-09-04 21:00:03 +0900

    live

--- Flutter/flutter_fire_video_30/lib/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_30/constants.dart';
-
-ThemeData theme() {
-  return ThemeData(
-      primaryColor: OssPrimaryColor,
-      scaffoldBackgroundColor: OssPrimaryColor,
-      fontFamily: "assets/fonts/PatuaOne-Regular.ttf");
-}
--- Flutter/flutter_live/android/app/src/main/kotlin/com/example/flutter_live/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_live
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
