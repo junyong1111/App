d8cc495a75d5defccb5b01d4838fd1c0c98b86b9
Junyong <jypark93@kookmin.ac.kr>
2022-09-16 16:36:03 +0900

    add:flutter

---
+++ Flutter/flutter_cached_images/lib/Pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl: "https://picsum.photos/250?image=9",
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
+      ),
+    );
+  }
+}
